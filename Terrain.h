924bf67515a031b471dfb46bd5098c856ee61aa2
Salvatore Tosti <saltosti@gmail.com>
2014-12-21 15:57:05 -0800

    Added Terrain.h

---
+++ Terrain.h
@@ -0,0 +1,17 @@
+/* Terrain.h */
+
+#ifndef TERRAIN_H
+#define TERRAIN_H
+
+#include <string>
+
+using namespace std;
+
+class Terrain {
+public:
+	Terrain();
+	~Terrain();
+	
+	String getName(void);
+	
+}
