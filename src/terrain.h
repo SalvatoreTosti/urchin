6522ed2cc5f1149a8011ab2786f2fc6ec2df520d
Salvatore Tosti <saltosti@gmail.com>
2014-12-21 16:05:11 -0800

    Altered terrain.h

---
+++ src/terrain.h
@@ -0,0 +1,17 @@
+/* terrain.h */
+
+#ifndef TERRAIN_H
+#define TERRAIN_H
+
+#include <string>
+
+using namespace std;
+
+class terrain {
+public:
+	terrain();
+	~terrain();
+	
+	string getName(void);
+	
+}
