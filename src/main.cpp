e06d7433e3b74f9500df7dca3890dfaf3a403748
Salvatore Tosti <saltosti@gmail.com>
2014-12-21 18:54:51 -0800

    adding testing to main

--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,22 @@
 #include <iostream>
 #include "terrain.h"
+#include "unit.h"
 #include "tile.h"
 
 using namespace std;
 
 int main(){
+    
+    
+    //Adhoc unit testing
+    //terrain tests
+   
+    terrain t;   
+ 
+    //unit tests
 
+    //tile tests
+
+    
     return 0;
 }
