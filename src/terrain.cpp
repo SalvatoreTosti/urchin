e9d137fe81735c60d73c88de49dec293dbbd965b
atost001 <atost001@ucr.edu>
2014-12-21 18:09:15 -0800

    Added terrain.cpp

---
+++ src/terrain.cpp
@@ -0,0 +1,16 @@
+#include "terrain.h"
+
+//Default constructs with empty params
+terrain::terrain(){
+    name = empty_Terrain;
+    offensiveBonus = empty_Offensive_Bonus;
+    defensiveBonus = empty_Defensive_Bonus;
+}
+
+//Destructor does nothing, no mem allocated
+terrain::~terrain(){
+}
+
+string getName(terrain t){
+    return t.name;
+}
