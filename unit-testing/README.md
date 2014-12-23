37b23b3f571f7806ed393acd21252184e732e015
Salvatore Tosti <saltosti@gmail.com>
2014-12-22 16:28:10 -0800

    Added makefile and README.md for unit-testing folder

--- unit-testing/README.md
+++ unit-testing/README.md
@@ -1,2 +1,5 @@
 #Unit Testing
 Unit tests for Urchin are based on the catch.hpp file provided from https://github.com/philsquared/Catch.
+
+Compiling and executing the main-test.cpp will run unit tests.
+Note: The makefile in the unit-testing folder only cleans the unit-test and unit-test.o files, full recompilation has to be triggered seperately from the src folder makefile.
--- unit-testing/test-main.cpp
+++ unit-testing/test-main.cpp
@@ -1,12 +1,20 @@
 #define CATCH_CONFIG_MAIN
 #include "catch.hpp"
+#include "../src/terrain.h"
 
 unsigned int Factorial (unsigned int number ){
     return number <= 1 ? number: Factorial(number-1)*number; }
 
 TEST_CASE( "Factorials are computed","[factorial]" ){
     REQUIRE( Factorial(1) == 1 ); 
-    REQUIRE( Factorial(2) == 3 );}
+    REQUIRE( Factorial(2) == 2 );}
 
-TEST_CASE( "Addition of 2 numbers","[additon]" ){
-    REQUIRE( (1 + 2) == 3);}
+//TEST_CASE( "Addition of 2 numbers","[additon]" ){
+//    REQUIRE( (1 + 2) == 3);}
+
+TEST_CASE( "getters return terrain values","[terrain]"){
+    terrain t;
+    SECTION( "getting values from default returns valid values"){
+        REQUIRE( t.getName() == "");
+        }
+    }
