b2229a772207421c158407f2ba75aa75def69169
SalvatoreTosti <saltosti@gmail.com>
2014-12-25 15:17:29 -0800

    added terrain factory method to git

---
+++ src/terrain_factory.h
@@ -0,0 +1,21 @@
+/* terrain_factory.h */
+
+#ifndef TERRAIN_FACTORY_H
+#define TERRAIN_FACTORY_H
+
+#include <string>
+
+using namespace std;
+
+class terrain_factory{
+
+public:
+    terrain_factory();
+    ~terrain_factory();
+    
+    makePlains();
+
+private:
+
+};
+#endif
