47c31c4a511e070b46451bbcb63ce7cefb930120
atost001 <atost001@ucr.edu>
2014-12-25 16:17:57 -0800

    added unit_factory and tank constructor to unit class

---
+++ src/unit_factory.h
@@ -0,0 +1,22 @@
+/* unit_factory.h*/
+
+#ifndef UNIT_FACTORY_H
+#define UNIT_FACTORY_H
+
+#include <string>
+#include "unit.h"
+
+using namespace std;
+
+class unit_factory{
+
+public:
+    unit_factory();
+    ~unit_factory();
+
+    unit makeTank();
+
+private:
+
+};
+#endif
