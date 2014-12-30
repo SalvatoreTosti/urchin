99744248ee4b5f03c81aa7eb87ef109012eb0930
SalvatoreTosti <saltosti@gmail.com>
2014-12-29 16:11:41 -0800

    jcreated a dual-system, osx and ubuntu, for SDL. Tested on ubuntu

---
+++ src/SDL/main.cpp
@@ -0,0 +1,57 @@
+#include <SDL2/SDL.h>
+#include <stdio.h>
+
+const int SCREEN_WIDTH = 640;
+const int SCREEN_HEIGHT = 480;
+
+SDL_Window* gWindow = NULL;
+SDL_Surface* gScreenSurface = NULL;
+SDL_Surface* gHelloWorld = NULL;
+
+bool init();
+bool loadMedia();
+void close();
+
+int main( int argc, char* args[]){
+    if(!init()){ printf("Failed to initialize!\n");}
+    else{
+        if(!loadMedia()){printf("Failed to load media\n");}
+        else{ SDL_BlitSurface(gHelloWorld,NULL,gScreenSurface,NULL);
+        SDL_UpdateWindowSurface(gWindow);
+        SDL_Delay(2000);
+        }
+    }
+    close();
+    return 0;
+}
+
+bool init(){
+    bool success = true;
+    if (SDL_Init(SDL_INIT_VIDEO) <0){
+        printf("SDL could not initialize! SDL_Error: %s\n",SDL_GetError());
+    success = false;}
+    else{
+        gWindow = SDL_CreateWindow("SDL Tutorial",SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, SCREEN_WIDTH, SCREEN_HEIGHT, SDL_WINDOW_SHOWN);
+        if(gWindow == NULL){
+            printf("Window could not be created! %s\n",SDL_GetError());
+            success = false;}
+        else{gScreenSurface = SDL_GetWindowSurface(gWindow);}
+    return success;}
+}
+
+bool loadMedia(){
+    bool success = true;
+    gHelloWorld= SDL_LoadBMP("hello.bmp");
+    if(gHelloWorld == NULL){
+        printf("Unable to load image. SDL Error: %s\n",SDL_GetError());
+        success = false;}
+    return success;}
+
+void close(){
+    SDL_FreeSurface(gHelloWorld);
+    gHelloWorld = NULL;
+    
+    SDL_DestroyWindow(gWindow);
+    gWindow = NULL;
+
+    SDL_Quit();} 
