b9d730e6e51fd5fe59b193bbc4c9be5ead86af0a
SalvatoreTosti <saltosti@gmail.com>
2016-01-02 11:11:47 -0800

    Added some basic events to main loop of SDL

--- src/SDL/main.cpp
+++ src/SDL/main.cpp
@@ -1,19 +1,49 @@
 #include <SDL2/SDL.h>
 //#include <sdl.h> //<SDL2/SDL.h> equivalent for OSX
 #include <stdio.h>
-
 #include "render.h"
-//const int SCREEN_WIDTH = 640;
-//const int SCREEN_HEIGHT = 480;
 
-//SDL_Window* gWindow = NULL;
-//SDL_Surface* gScreenSurface = NULL;
-//SDL_Surface* gHelloWorld = NULL;
+using namespace std;
 
-//bool init();
-//bool loadMedia();
-//void close();
+/* render logic based on lazyfoo.net/tutorials/SDL/03_event_driven_programming/index.php */
+ 
+int main( int argc, char* args[]){
+    render render;
+    if(!render.init()){ 
+        printf("Failed to initialize.\n");
+    }
+    else {
+        //do nothing
+    }
+    if(!render.loadMedia()){
+        printf("Failed to load media.\n");
+    }
+    else {
+        //do nothing
+    }
+    
+    bool quit = false;
+    SDL_Event e;
+    while( !quit ){
+        while( SDL_PollEvent( &e ) != 0){
+            if( e.type == SDL_QUIT ){
+                quit = true;
+            }    
+            else {
+                //do nothing
+            } 
+        } 
+        
+        SDL_BlitSurface( render.getDisplaySurface(), NULL, render.getSDLScreenSurface(), NULL );
+        SDL_UpdateWindowSurface(render.getSDLWindow());
+    } 
+    render.close();
+    return 0;
+}
 
+
+//render proof of concept
+/*
 int main( int argc, char* args[]){
     render render; 
     if(!render.init()){ printf("Failed to initialize!\n");}
@@ -27,35 +57,4 @@ int main( int argc, char* args[]){
     render.close();
     return 0;
 }
-/*
-bool init(){
-    bool success = true;
-    if (SDL_Init(SDL_INIT_VIDEO) <0){
-        printf("SDL could not initialize! SDL_Error: %s\n",SDL_GetError());
-    success = false;}
-    else{
-        gWindow = SDL_CreateWindow("SDL Tutorial",SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, SCREEN_WIDTH, SCREEN_HEIGHT, SDL_WINDOW_SHOWN);
-        if(gWindow == NULL){
-            printf("Window could not be created! %s\n",SDL_GetError());
-            success = false;}
-        else{gScreenSurface = SDL_GetWindowSurface(gWindow);}
-    return success;}
-}
-
-bool loadMedia(){
-    bool success = true;
-    gHelloWorld= SDL_LoadBMP("hello.bmp");
-    if(gHelloWorld == NULL){
-        printf("Unable to load image. SDL Error: %s\n",SDL_GetError());
-        success = false;}
-    return success;}
-
-void close(){
-    SDL_FreeSurface(gHelloWorld);
-    gHelloWorld = NULL;
-    
-    SDL_DestroyWindow(gWindow);
-    gWindow = NULL;
-
-    SDL_Quit();} 
 */
